840a524abae337ada91746ba1930b015f8ac9b47
elBichon <reuglewiczjeanedouard@gmail.com>
2021-01-31 22:11:58 +0100

    testing pygithub$

---
+++ test.py
@@ -0,0 +1,26 @@
+
+
+
+from github import Github
+
+
+# or using an access token
+g = Github("dbdb459a70a7ec73bcb2707d71593f5acf1fe091")
+path = "elBichon/webex_experiments"
+
+repo = g.get_repo(path)
+print(repo.get_topics())
+print(repo.stargazers_count)
+
+
+open_issues = repo.get_issues(state='open')
+for issue in open_issues:
+	print(issue)
+
+labels = repo.get_labels()
+for label in labels:
+	print(label)
+
+contents = repo.get_contents("")
+for content_file in contents:
+	print(content_file)
