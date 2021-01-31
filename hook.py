51ac57af1d4662b7c519c0a5dccb32ac31e0140b
elBichon <reuglewiczjeanedouard@gmail.com>
2021-01-31 22:30:06 +0100

    testing webhooks in github

---
+++ hook.py
@@ -0,0 +1,20 @@
+import os
+import json
+import requests
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+
+GITHUB_SECRET = "dbdb459a70a7ec73bcb2707d71593f5acf1fe091"
+
+
+@app.route("/payload", methods=['POST'])
+def github_webhook_endpoint():
+	if request.headers['Content-Type'] == 'application/json':
+		return json.dumps(request.json)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
