4e7ffce4a53444885002a9c1fa36e07dba68bdd0
elBichon <reuglewiczjeanedouard@gmail.com>
2021-01-31 22:33:25 +0100

    adding display of the payload

--- hook.py
+++ hook.py
@@ -12,7 +12,9 @@ GITHUB_SECRET = "dbdb459a70a7ec73bcb2707d71593f5acf1fe091"
 @app.route("/payload", methods=['POST'])
 def github_webhook_endpoint():
 	if request.headers['Content-Type'] == 'application/json':
-		return json.dumps(request.json)
+		data = json.dumps(request.json)
+		print(data)
+		return(data)
 
 
 if __name__ == "__main__":
